0f5d60c12455b658a517c6013e185d45830f1c13
jaenwawe <nwawe.jae@gmail.com>
2021-10-01 21:34:18 -0500

    commit tree 159ec4a1471314fb8bdf464a729c35803f3c7799

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,13 @@
 class Game < ActiveRecord::Base
 
+
+    def self.order
+        binding.pry
+       # self.order(:price)
+       self.limit(5)
+       #!!self.game.find_by(title: self.title)
+    end
+
+
+
 end
